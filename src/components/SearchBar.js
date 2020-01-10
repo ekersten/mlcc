b0cd04c6eb39a9cc3070233c0655cd96a4f52924
Eric Kersten <ekersten@gmail.com>
2020-01-09 21:49:58 -0300

    search bar assets y estilos

---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Logo from '../assets/img/Logo_ML.png';
+import SearchIcon from '../assets/img/ic_Search.png';
+import '../sass/components/SearchBar.scss';
+
+const SearchBar = props => {
+    return (
+        <div className="search-bar">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-1 offset-md-1">
+                        <img src={Logo} className="logo" alt="Mercado Libre" />
+                    </div>
+                    <div className="col-md-9">
+                        <form action="/items">
+                            <input type="text" name="search" placeholder="Nunca dejes de buscar" />
+                            <button><img src={SearchIcon} alt="Buscar" /></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SearchBar;
