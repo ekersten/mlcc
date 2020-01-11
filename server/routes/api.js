fe55e75980a1548c97a1ac50bd7ee799f64b9b68
Eric Kersten <ekersten@gmail.com>
2020-01-10 21:41:57 -0300

    fix categorias

--- server/routes/api.js
+++ server/routes/api.js
@@ -27,7 +27,7 @@ router.get('/items', function (req, res) {
                 name: 'Eric',
                 lastname: 'Kersten'
             },
-            categories: response.data.available_filters[0].values.map(category => category.name),
+            categories: response.data.filters[0].values[0].path_from_root.map(category => category.name),
             items: response.data.results.map(item => ({
                 id: item.id,
                 title: item.title,
@@ -59,26 +59,30 @@ router.get('/items/:id', function (req, res) {
         axios.get(`https://api.mercadolibre.com/items/${id}`),
         axios.get(`https://api.mercadolibre.com/items/${id}/description`)
     ]).then(([detailResponse, descriptionResponse]) => {
-        res.json({
-            author: {
-                name: 'Eric',
-                lastname: 'Kersten'
-            },
-            item: {
-                id: detailResponse.data.id,
-                title: detailResponse.data.title,
-                price: {
-                    currency: detailResponse.data.currency_id,
-                    amount: Math.floor(detailResponse.data.price),
-                    decimals: Math.floor((detailResponse.data.price - Math.floor(detailResponse.data.price)) * 100)
+        axios.get(`https://api.mercadolibre.com/categories/${detailResponse.data.category_id}`).then(categoryResponse => {
+            res.json({
+                author: {
+                    name: 'Eric',
+                    lastname: 'Kersten'
                 },
-                picture: detailResponse.data.pictures[0].secure_url,
-                condition: detailResponse.data.condition,
-                free_shipping: detailResponse.data.shipping.free_shipping,
-                sold_quantity: detailResponse.data.sold_quantity,
-                description: descriptionResponse.data.plain_text
-            }
+                item: {
+                    id: detailResponse.data.id,
+                    title: detailResponse.data.title,
+                    category: categoryResponse.data,
+                    price: {
+                        currency: detailResponse.data.currency_id,
+                        amount: Math.floor(detailResponse.data.price),
+                        decimals: Math.floor((detailResponse.data.price - Math.floor(detailResponse.data.price)) * 100)
+                    },
+                    picture: detailResponse.data.pictures[0].secure_url,
+                    condition: detailResponse.data.condition,
+                    free_shipping: detailResponse.data.shipping.free_shipping,
+                    sold_quantity: detailResponse.data.sold_quantity,
+                    description: descriptionResponse.data.plain_text
+                }
+            });
         });
+        
     }).catch(error => {
         res.status(error.response.status).json({
             error: error.response.data.error,
