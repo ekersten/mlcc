c80653463ecf6c478502e69557d3fe2220e0ec9e
Eric Kersten <ekersten@gmail.com>
2020-01-11 08:20:27 -0300

    estilos base pagina detalle

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
+import '../sass/pages/DetailPage.scss';
+
 import SearchBar from '../components/SearchBar';
 import Container from '../components/Container';
 import Breadcrumbs from '../components/Breadcrumbs';
@@ -35,12 +37,29 @@ const DetailPage = () => {
             <Container>
                 <div className="row">
                     <div className="col-12">
-                        <h4>Detalle {id}</h4>
                         {loading ? <p>Cargando...</p> : null}
                         {item ? (
                             <>
-                                <h1>{item.title} (${item.price.amount},{item.price.decimals} {item.price.currency.toUpperCase()})</h1>
-                                <p>{item.description}</p>
+                                <div className="row">
+                                    <div className="col-8">
+                                        <img src={item.picture} alt={item.title}/>
+                                    </div>
+                                    <div className="col-4">
+                                        <div className="sold">{item.condition === 'new' ? 'Nuevo' : 'Usado'} - {item.sold_quantity} {item.sold_quantity != 1 ? 'vendidos' : 'vendido'}</div>
+                                        <div className="title"><h1>{item.title}</h1></div>
+                                        <div className="price">
+                                            {item.price.currency === 'USD' ? 'U$S' : '$'} {item.price.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")} {item.price.decimals ? <span className="decimals">.{item.price.decimals}</span> : null}
+                                        </div>
+                                        <div className="buy">
+                                            <button className="btn">Comprar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row col-md-8 description">
+                                    <h2>Descripción del producto</h2>
+                                    <p>{item.description}</p>
+                                </div>
+                                
                             </>
                         ) : null}
                     </div>
