b705eadfde0de5b9cc76d25ccd7999ff4e3c6dfc
Eric Kersten <ekersten@gmail.com>
2020-01-11 14:13:17 -0300

    envio gratis

--- src/components/ResultsListItem.js
+++ src/components/ResultsListItem.js
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom';
 
 import '../sass/components/ResultsListItem.scss';
 
+import IconShipping from '../assets/img/ic_shipping.png';
+
 const ResultsListItem = ({item}) => {
 
     return (
         <Link to={`/items/${item.id}`} className="results-list-item">
             <div className="item-image" style={{ backgroundImage: `url(${item.picture})`}}></div>
             <div className="info">
-                <div className="price">{item.price.currency === 'USD' ? 'U$S' : '$'} {item.price.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")} {item.price.decimals ? <span className="decimals">.{item.price.decimals}</span> : null}</div>
+                <div className="price">
+                    {item.price.currency === 'USD' ? 'U$S' : '$'} {item.price.amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")} {item.price.decimals ? <span className="decimals">.{item.price.decimals}</span> : null}
+                    {item.free_shipping ? (
+                        <img src={IconShipping} className="free-shipping" alt="Envío gratis"/>
+                    ) : null}    
+                </div>
                 <div className="title">{item.title}</div>
             </div>
             <div className="location">
